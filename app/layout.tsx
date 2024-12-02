48d052c76ec79c6b3f5bd13435a27cbfb5c03c1b
Sourabh <sourabhsinha396@gmail.com>
2024-12-02 21:30:00 +0530

    setup shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import NavBar from "./NavBar";
+import NavBar from "@/components/ui/navbar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Issue-Wissue",
@@ -26,8 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      <body className="dark"
       >
         <NavBar />
         <main>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+import NavBar from "@/components/ui/navbar";
 
 export default function Home() {
   return (
     <div>
+      <div className="flex items-center justify-center flex-col">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Taxing Laughter: The Joke Tax Chronicles
+        </h1>
+        <Button className="mt-4">Click me</Button>
+      </div>
     </div>
   );
 }
